ce91460605f670ee712762e29e7ddfc526864f10
SurasakDiskul <pukpik1337@gmail.com>
2023-08-19 16:38:44 +0700

    commit

--- sidebar.php
+++ sidebar.php
@@ -2,6 +2,124 @@
 session_start();
 include('./php/connect.php');
 ?>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Encyclopedia - CJ Auto</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="./2.png" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="css/styles.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.13.1/b-2.3.3/r-2.4.0/datatables.min.css" />
+    <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.13.1/b-2.3.3/r-2.4.0/datatables.min.js"></script>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Kanit:wght@200&display=swap');
+body {
+  line-height: 1.8;
+  font-family: 'Kanit', sans-serif;
+    -webkit-font-smoothing: antialiased;
+  font-size: 16px;
+  color: rgba(0, 0, 0, 0.6);
+}
+
+h1, .h1, h2, .h2, h3, .h3, h4, .h4, h5, .h5, h6, .h6 {
+  font-family: 'Kanit', sans-serif;
+    font-weight: 700;
+  color: #232323;
+  letter-spacing: 0.5px;
+}
+
+h1, .h1 {
+  font-size: 48px;
+  text-transform: uppercase;
+}
+
+h2, .h2 {
+  font-size: 38px;
+  text-transform: uppercase;
+}
+
+h3, .h3 {
+  font-size: 28px;
+  line-height: 38px;
+  text-transform: uppercase;
+}
+
+h4, .h4 {
+  font-size: 22px;
+  line-height: 30px;
+}
+
+h5, .h5 {
+  font-size: 18px;
+  line-height: 24px;
+}
+
+h6, .h6 {
+  font-size: 16px;
+  text-transform: uppercase;
+  font-weight: 400;
+}
+.p {
+  font-family: 'Kanit', sans-serif;}
+  .b {
+  font-family: 'Kanit', sans-serif;}
+  .a {
+  font-family: 'Kanit', sans-serif;}
+.font-primary {
+  font-family: 'Kanit', sans-serif;}
+
+.font-secondary {
+  font-family: 'Kanit', sans-serif;}
+        header {
+            background-image: url("./CJ333GARAGE.jpg");
+            background-repeat: no-repeat;
+            background-position: center center;
+            background-attachment: fixed;
+            -o-background-size: 100% 100%, auto;
+            -moz-background-size: 100% 100%, auto;
+            -webkit-background-size: 100% 100%, auto;
+            background-size: 100% 100%, auto;
+        }
+
+        p.card-text {
+            font-family: 'Kanit', sans-serif;
+            white-space: nowrap;
+            width: 300px;
+            overflow: hidden;
+            text-overflow: ellipsis;
+        }
+    </style>
+
+    <style type="text/css">
+        div.img-resize img {
+            width: 854px;
+            height: auto;
+        }
+
+        div.img-resize {
+            width: auto;
+            height: 300px;
+            overflow: hidden;
+            text-align: center;
+        }
+
+        div.img-resize1 img {
+            width: 500;
+            height: auto;
+        }
+
+        div.img-resize1 {
+            width: auto;
+            height: 200px;
+            overflow: hidden;
+            text-align: center;
+        }
+    </style>
+</head>
 <!-- Side widgets-->
 <div class="col-lg-4">
     <div class="card mb-4">
